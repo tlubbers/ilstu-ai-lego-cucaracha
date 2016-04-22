c46ffc323956831222ec592bf0a09875f99f8f93
Tyler Lubbers <tlubber@ilstu.edu>
2016-04-21 21:05:14 -0500

    Syntax fix for initial test run

--- cucaracha-playground.py
+++ cucaracha-playground.py
@@ -13,10 +13,36 @@ from time         import sleep
 from random       import choice, randint
 from ev3dev.auto  import *
 
-# Binding two large motors plugged into PortB and PortC
-# motors = [LargeMotor(address) for address in (OUTPUT_B, OUTPUT_C)]
+# Bind two large motors plugged into PortB and PortC to motors
+#motors = [LargeMotor(address) for address in (OUTPUT_B, OUTPUT_C)]
+
+# Bind infrared and touch to ir and ts, respectively
+#ir = InfraredSensor(); assert ir.connected
+#ts = TouchSensor();    assert ts.connected
+
+# Infared in proximity mode lets you use things like d = ir.value(), if d > 50
+# than move forward.
+#ir.mode = 'IR-PROX'
+
+# Bind buttons on the brick to btn, a Button() object is more like a
+# button controller than a button.
+btn = Button()
 
 def start():
   Sound.speak("La cucaracha!");
-  for m in motors
-    m.rundirect()
+  # Putting motors in rundirect lets you change speeds later
+  #for m in motors
+  #  m.rundirect()
+
+def playMusic():
+    # Eventually will incorporate song using Sound objects
+    Sound.tone([(1000, 500, 500)] * 3)
+
+start()
+while not btn.any():
+  start()
+  playMusic()
+
+# At exit stop motors
+#for m in motors:
+  #  m.stop()
